5e7fe9c507cbb24655945df98b7772e5a393428e
Jared Roberts <jared.roberts@rubiconglobal.com>
2018-07-18 11:44:50 -0700

    Add project files.

---
+++ Rubicon.Console.EndPointPing/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rubicon.Console.EndPointPing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
